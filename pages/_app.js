bad5ba63fb563501131a8d2ea47e8118f66a7dd6
Handai-Yamato <y,handai@crmlab.jp>
2022-12-15 14:18:06 +0900

    update:Emotionの追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import createEmotionCache from "../libs/createEmotionCache";
+import PropTypes from "prop-types";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { Global } from "@emotion/react";
+import Layout from "../components/layouts/layout";
+import { style } from "../styles/globals";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Layout>
+        <Global styles={style} />
+        <Component {...pageProps} />
+      </Layout>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
